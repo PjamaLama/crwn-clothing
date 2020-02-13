3b2a00b246adec854ac3d79d165e9d6b3df5a699
Marnu <MarnuvanNiekerk@derivco.co.za>
2020-02-13 15:25:21 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
+import React, { Component } from "react";
+import SHOP_DATA from "./shop.data.js";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 export class ShopPage extends Component {
-    constructor(props){
-        super(props);
-        
-        this.state={
-            collections: SHOP_DATA    
-        }
-    }
+	constructor(props) {
+		super(props);
 
-    render() {
-        const {collections} = this.state;
-        return (<div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-            </div>
-        )
-    }
+		this.state = {
+			collections: SHOP_DATA
+		};
+	}
+
+	render() {
+		const { collections } = this.state;
+		return (
+			<div className="shop-page">
+				{collections.map(({ id, ...otherCollectionProps }) => (
+					<CollectionPreview key={id} {...otherCollectionProps} />
+				))}
+			</div>
+		);
+	}
 }
 
 export default ShopPage;
